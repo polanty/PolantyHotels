01fdb2fd8d61c340a5ca13e572d1f7dc47f19a0d
polanty <Tijaniabiola1599@gmail.com>
2025-12-01 15:13:04 -0800

    imported the dev-data

--- app.js
+++ app.js
@@ -19,6 +19,13 @@ app.use(express.json());
 
 app.use("/api/v1/hotels", hotelRouter);
 
+//Global Non-existing route error handling middleware
+app.use((req, res, next) => {
+  res.status(404).json({
+    status: "fail",
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
 // app.use((err, req, res, next) => {
 //   console.error(err.stack);
 //   res.status(500).send("Something broke!");
--- src/Routes/HotelsRoutes/HotelRoute.js
+++ src/Routes/HotelsRoutes/HotelRoute.js
@@ -1,4 +1,6 @@
 import express from "express";
+// import { fileURLToPath } from "url";
+// import { dirname } from "path";
 
 //loading environment variables
 import dotenv from "dotenv";
@@ -6,22 +8,44 @@ dotenv.config();
 
 const hotelRouter = express.Router();
 
-hotelRouter.route("/").get(async (req, res) => {
-  try {
-    // throw new Error("Database connection failed"); //
-    res.status(200).json({
-      status: "success",
-      data: {
-        hotels: ["Hotel One", "Hotel Two", "Hotel Three"],
-      },
-    });
-  } catch (error) {
-    res.status(500).json({
-      status: "error",
-      message: "An error occurred while fetching hotels.",
-    });
-  }
-});
+// Recreate __filename and __dirname
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+
+hotelRouter
+  .route("/")
+  .get(async (req, res) => {
+    try {
+      // throw new Error("Database connection failed"); //
+      res.status(200).json({
+        status: "success",
+        data: {
+          hotels: ["Hotel One", "Hotel Two", "Hotel Three"],
+        },
+      });
+    } catch (error) {
+      res.status(500).json({
+        status: "error",
+        message: "An error occurred while fetching hotels.",
+      });
+    }
+  })
+  .post(async (req, res) => {
+    try {
+      const newHotel = req.body;
+      res.status(201).json({
+        status: "success",
+        data: {
+          hotel: newHotel,
+        },
+      });
+    } catch (error) {
+      res.status(500).json({
+        status: "error",
+        message: "An error occurred while creating the hotel.",
+      });
+    }
+  });
 
 hotelRouter
   .route("/:id")
@@ -56,6 +80,20 @@ hotelRouter
       res.status(500).json({
         status: "error",
         message: "An error occurred while creating the hotel.",
+      });
+    }
+  })
+  .delete(async (req, res) => {
+    const hotelId = req.params.id;
+    try {
+      res.status(204).json({
+        status: "success",
+        data: null,
+      });
+    } catch (error) {
+      res.status(500).json({
+        status: "error",
+        message: "An error occurred while deleting the hotel.",
       });
     }
   });
